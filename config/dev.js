ae60694fc3a5d6c5aca3dee5fbf390df7d577bb7
star sky <1176789241@qq.com>
2020-03-11 08:23:47 +0800

    1. 添加oneOf优化打包配置 2. 修改demo代码

--- config/dev.js
+++ config/dev.js
@@ -15,53 +15,58 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
+                oneOf: [
+                    {
+                        test: /\.css$/,
+                        use: [
+                            'style-loader',
+                            'css-loader'
+                        ]
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        loader: 'babel-loader',
+                        options: {
+                            "presets": [
+                                ["@babel/preset-env", {
+                                    useBuiltIns: 'usage',
+                                    // 指定core-js版本
+                                    corejs: {
+                                        version: 3
+                                    },
+                                    // 指定兼容性到那个版本浏览器
+                                    targets: {
+                                        "chrome": "60",
+                                        "firefox": "60",
+                                        "ie": "9",
+                                        "safari": "10",
+                                        "edge": "17"
+                                    }
+                                }]
+                            ]
+                        }
+                    },
+                    {
+                        test: /\.(png|gif|jpg|jpeg)$/,
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8 * 1024,
+                            esModule: false
+                        }
+                    },
+                    {
+                        test: /\.html$/,
+                        loader: 'html-loader'
+                    },
+                    {
+                        exclude: /\.(html|css|js|png|gif|jpeg|jpg)/,
+                        loader: 'file-loader'
+                    }
                 ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                options: {
-                    "presets": [
-                        ["@babel/preset-env", {
-                            useBuiltIns:'usage',
-                            // 指定core-js版本
-                            corejs:{
-                                version:3
-                               },
-                            // 指定兼容性到那个版本浏览器
-                            targets: {
-                                "chrome": "60",
-                                "firefox": "60",
-                                "ie": "9",
-                                "safari": "10",
-                                "edge": "17"
-                            }
-                        }]
-                    ]
-                }
-            },
-            {
-                test: /\.(png|gif|jpg|jpeg)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 8 * 1024,
-                    esModule: false
-                }
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
-            {
-                exclude: /\.(html|css|js|png|gif|jpeg|jpg)/,
-                loader: 'file-loader'
             }
         ]
+
     },
 
     plugins: [
@@ -81,7 +86,7 @@ module.exports = {
         }
     },
 
-    devtool:'source-map',
+    devtool: 'source-map',
 
     mode: 'development'
 
@@ -89,11 +94,11 @@ module.exports = {
 
 /**
  * webpack dev 配置所要实现的目标
- * 
- * 1. 打包速度更快 
- *     
+ *
+ * 1. 打包速度更快
+ *    1. oneOf 在oneOf中只使用其中的一个loader
  * 2. 更加方便开发人员调试
  *    1. HMR 热模块替换 局部刷新
  *    2. source-map技术  通过构建后代码可以追踪到源代码
- *   
+ *
  */
