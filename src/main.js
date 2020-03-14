0e79e58f2056f9e728417b6e856718cc8f89c38a
Re_star <44056372+trueLoving@users.noreply.github.com>
2020-03-14 14:29:41 +0800

    Revert "1. 添加不同开发需求下的配置方案"

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-import Vue from "vue";
+import './style/a.css';
+import './style/b.css';
+import { add } from './print'
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
-import App from './App.vue';
-import router from './router';
-// import store from './store';
+console.log("hello world");
 
-Vue.use(ElementUI);
+add(2,1)
 
-new Vue({
-    el: "#app",
-    router,
-    render: h => h(App)
-})
+/**
+ * 当我们开启HMR功能时，则module对象上会有一个hot更新
+ * 而对于js的HMR，我们只能针对那些非入口js文件
+ * 因为一旦入口的js文件发生变化，就意味着依赖关系图发生了变化，只能全体重新打包加载
+ */
+if (module.hot) {
+
+    module.hot.accept('./print.js', function () {
+        console.log("hello world")
+        add(1, 2);
+        // 该方法会监视print.js的变化，一旦发生变化,其他模块默认不会重新打包构建，只会重新加载该模块
+    })
+
+}
